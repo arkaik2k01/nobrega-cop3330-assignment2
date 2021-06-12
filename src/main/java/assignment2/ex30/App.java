24db33d7b0818b71f02236f0631623b3498ec8e7
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-06-12 15:45:24 -0400

    Done ex30. ez.

--- src/main/java/assignment2/ex30/App.java
+++ src/main/java/assignment2/ex30/App.java
@@ -36,6 +36,12 @@ public class App
 {
     public static void main(String[] args)
     {
-
+        //I mean, this was pretty simple...
+        for(int i = 1; i < 13; i++) {
+            for (int j = 1; j < 13; j++) {
+                System.out.printf("%4d ", i * j);
+            }
+            System.out.println();
+        }
     }
 }
